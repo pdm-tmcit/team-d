69fa25e84ef926f5c4a09a2ab3f2d9dbd9977127
S.S <m16072@g.metro-cit.ac.jp>
2020-09-07 01:38:07 +0900

    main.py加筆

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import module.sentence_module as sentence
 import module.status_module as status
 import module.io_module as io
 import module.token_module as tokens
+import module.name_module as name
 import module.situation_module as situation
 
 if __name__ == "__main__":
@@ -18,6 +19,27 @@ if __name__ == "__main__":
         exit
 
     status.init()
+    tokenize_csv = tokens.token(csv)
+
+    names = name.name_setlist(csv)
+    nicknames = name.nickname_setlist(tokenize_csv)
+    names_dict = name.name_dict(name.name_similar_nickname(names,nicknames))
+
+    #print("### names_dict ###")
+    #print(names_dict)
+
+    for n in names:
+      status.save("プレイヤー名ID",str(names.index(n)),n)
+      status.save("プレイヤー名",n,str(names.index(n)))
+      status.save("プレイヤーニックネーム",str(names.index(n)),names_dict[n][0])
+
+    #for id in range(len(names)):
+    #  print(id,status.load("プレイヤー名ID",str(id)))
+    #  print(id,status.load("プレイヤーニックネーム",str(id)))
+
+    #print("### CO_check ###")
+    situation.CO_check(csv)
+
     #print(tokenize_csv)
     #print(terget_role)
     output_sentence = sentence.sentence_moddule(terget_role)

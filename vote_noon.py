dc482c3dbc9c06cfe91bad1f10742c23ad132fe1
aaaakkkkiiii <theaki14@gmail.com>
2020-12-08 09:14:27 +0900

    :)

--- vote_noon.py
+++ vote_noon.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import sys
 
 
 class Character:
@@ -14,33 +15,34 @@ class Character:
 
 
 my_character = None
-character_list = []
 
 
 def main():
     url = "https://werewolf.world/village/example/0.3/client2server/noonVote.jsonld"
+    args = sys.argv
     response = requests.get(url)
     json_data = response.json()
     get_phase_noon()
-    print(vote(json_data, 2))
+    print(vote(json_data, int(args[1]), get_phase_noon()))
 
 
+# 午後のフェーズのjsonを取得してキャラクターのリストを返す
 def get_phase_noon():
     url = "https://werewolf.world/village/example/0.3/server2client/noon.jsonld"
     response = requests.get(url)
     json_data = response.json()
+    character_list = []
 
     for character in json_data['character']:
         if character['isMine']:
             my_character = character
         character_list.append(character)
 
+    return character_list
 
-def vote(json_data, id):
-    """
-    jsonと投票する人のidを受け取ってvoteのjsonを生成する
-    """
 
+# jsonと投票する人のid、キャラクターのリストを受け取ってvoteのjsonを生成する
+def vote(json_data, id, character_list):
     for target in character_list:
         if target['id'] == id:
             json_data['character']['id'] = target['id']
@@ -48,7 +50,6 @@ def vote(json_data, id):
             json_data['character']['name']['ja'] = target['name']['ja']
             json_data['character']['image'] = target['image']
 
-    print(json_data)
     return json_data
 
 
